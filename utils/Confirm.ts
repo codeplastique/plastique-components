4281701c3dfeb6960d3bf446a13f5ca10deb7532
Bondarenko Artyom <bondarenko@protei.ru>
2021-05-11 18:37:08 +0300

    add Confirm.waitResolving()

--- utils/Confirm.ts
+++ utils/Confirm.ts
@@ -2,13 +2,36 @@ export default class Confirm<T>{
     protected readonly promise: Promise<T>
     protected resolve: (arg?: T) => void
     protected reject: (arg?: T) => void
-    constructor() {
+    protected readonly resolvePromise: Promise<T>
+
+
+    constructor(resolveAction?: () => Promise<any>) {
         this.promise = new Promise((res, rej) => {
             this.resolve = res;
             this.reject = rej;
         })
+        if(resolveAction){
+            let resolve: (arg?: any) => void
+            let reject: (arg?: any) => void
+
+            this.resolvePromise = new Promise((res, rej) => {
+                resolve = res;
+                reject = rej;
+            })
+            this.promise.then(
+                () => resolveAction().then(resolve, reject),
+                () => reject()
+            )
+        }
     }
     public wait(): Promise<T>{
         return this.promise;
+    }
+
+    /**
+     * waits for the resolveAction to be completed
+     */
+    public waitResolving(): Promise<T>{
+        return this.resolvePromise? this.resolvePromise: this.promise;
     }
 }
